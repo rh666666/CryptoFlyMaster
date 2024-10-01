b0ea0e6e65394313ba97d7768bc237652c8cbda8
rh666666 <54299404+rh666666@users.noreply.github.com>
2024-10-01 12:19:18 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-import model
-import argparse
+import argparse, subprocess, sys, pyfiglet
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
 
+import model.keyed_sub
 import model.affine
 import model.hill
-import model.keyed_sub
 import model.playfair
 import model.vigenere
 
+import UI.Ui_main
+
 def nogui_mode():
     while True:
+        subprocess.call("cls", shell=True)
+        print(pyfiglet.figlet_format('CryptoFlyMaster'))
+        print("输入相应数字选择模式：")
         print('''
-            输入相应数字选择模式：
             1. 使用密钥的单表代替密码
             2. 仿射密码
             3. 维几尼亚密码
@@ -35,11 +40,20 @@ def nogui_mode():
             print("Error: 请输入1-5.")
 
 def ui_mode():
-    print("不好意思哈，UI还没做……")
+    print("不好意思哈，UI还没做好……")
+    print("这边建议 python main.py --nogui 无UI启动程序呢~")
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    ui = UI.Ui_main.Ui_MainWindow()
+    ui.setupUi(win)
+    win.setWindowTitle('CryptoFlyMaster')
+    win.setWindowIcon(QIcon('image/icon.png'))
+    win.show()
+    sys.exit(app.exec_())
 
 def main():
-    parser = argparse.ArgumentParser(description='报错带师')
-    parser.add_argument('-nogui', action='store_true', help='使用字符界面')
+    parser = argparse.ArgumentParser(description='CryptoFlyMaster 一个古典密码工具箱')
+    parser.add_argument('--nogui', action='store_true', help='使用字符界面')
     args = parser.parse_args()
 
     if args.nogui:
