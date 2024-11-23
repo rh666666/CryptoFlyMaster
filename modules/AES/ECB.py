6b4a1483ab148f1213800867de61c6823b05ea32
rh666666 <787314057@qq.com>
2024-11-23 15:48:09 +0800

    update

--- modules/AES/ECB.py
+++ modules/AES/ECB.py
@@ -1,9 +1,9 @@
-from modules.DES import DES
-def pkcs7_pad(data: bytes, block_size: int = 8) -> bytes:
+from modules.AES import AES
+def pkcs7_pad(data: bytes, block_size: int = 16) -> bytes:
     """
     对数据进行 PKCS#7 填充。
     :param data: 输入的字节数据
-    :param block_size: 块大小，默认为 8（DES 的块大小）
+    :param block_size: 块大小，默认为 16（DES 的块大小）
     :return: 填充后的字节数据
     """
     padding_len = block_size - len(data) % block_size
@@ -34,18 +34,22 @@ def main():
         else:
             Text_hex = Text
 
-        Key = input("请输入密钥(64位,8字节):")
+        Key = input("请输入密钥(128位,16字节):") 
         Key_hex = ''.join(format(ord(char), '02x') for char in Key if char.isprintable())
-        if len(Key_hex) != 16:
-            print("密钥长度不为 64 位。")
+
+        if len(Key) != 16:
+            print("密钥长度不为 128 位。")
             return
         
         result = ''
-
-        for i in range(0, len(Text_hex),16):
-            temp_str1 = Text_hex[i:i+16]
-            result += DES.encryption(temp_str1, Key_hex) if choice == 1 else DES.decryption(temp_str1, Key_hex)
-
+  
+        for i in range(0, len(Text_hex),32):
+            temp_str1 = Text_hex[i:i+32]
+            result_list = AES.Encryption(temp_str1, Key_hex) if choice == 1 else AES.Decryption(temp_str1, Key_hex)
+            for i in range(4):
+                for j in range(4):
+                    result += result_list[j][i].lstrip("0x").zfill(2)
+  
         if result:
             if choice == 1:
                 print("加密后的密文:"+ result)
